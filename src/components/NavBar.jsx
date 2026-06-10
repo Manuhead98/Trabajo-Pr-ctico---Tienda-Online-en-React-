e11a9cd70cc405cd2935990e3b3d6fefee73c487
carogn <carolina.gonzalez070602@gmail.com>
2026-06-10 19:57:57 -0300

    mas cosas

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function NavBar() {
+function NavBar({ darkMode, setDarkMode }) {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
 
@@ -48,6 +48,13 @@ function NavBar() {
                         </Nav.Link>
 
                     </Nav>
+
+                    <Button
+                        variant={darkMode ? "light" : "outline-light"}
+                        onClick={() => setDarkMode(!darkMode)}
+                    >
+                        {darkMode ? "☀️" : "🌙"}
+                    </Button>
 
                 </Navbar.Collapse>
 
